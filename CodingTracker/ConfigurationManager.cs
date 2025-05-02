f214abb574a8d5de682dbadc2af54d3cb533929d
Marcin-Kumar <marcinkumar@gmail.com>
2025-05-02 16:05:10 +0200

    Added required files for the project and configuration code

---
+++ CodingTracker/ConfigurationManager.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CodingTracker;
+
+internal class ConfigurationManager
+{
+    private static ConfigurationManager? instance;
+
+    private IConfigurationRoot configurationRoot;
+
+    private ConfigurationManager() { 
+        configurationRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();     
+    }
+
+    public ConfigurationManager GetConfiguration()
+    {
+        instance ??= new ConfigurationManager();
+        return instance;
+    }
+
+    public string GetConnectionString()
+    {
+        string? connectionString = configurationRoot.GetConnectionString("DefaultConnection");
+        if (connectionString == null) {
+            throw new ArgumentNullException("SQLite connection string isn't configured");
+        }
+        return connectionString;
+    }
+
+}
